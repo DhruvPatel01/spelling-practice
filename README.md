db779551985f7fa4281d986f14fcbb7d3ae8189a
Dhruv Patel <dhruv.nanosoft@gmail.com>
2021-07-29 22:16:59 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+A quick spell check practice app written in vanilla-js.
+ 
+It uses webspeech API. I hate myself for using it but it was simple. So simple.
+
+This app didn't work in Firefox, neither in Mac nor in Linux. Does it work for you in Firefox? Let me know.
+
+It didn't even work in Chromium on Linux, didn't try on Chromium on Mac.
+
+It works in Chrome though. Also on Edge. In both OSes I checked.
+
+## How to use?
+
+Simple. Copy and paste the spellings you want to practice in the box. One word per line. Then click on `Populate` button. Now in the bottom, click on the `Play` button, a voice will speak the word. Your job is to write correct spelling in the box. Once correct spell has been written, text will turn in green, and it will reset after one second. A random word from the list will be picked next for you to repeat the exercise.
+
+In case you need are stuck, click on `View Correct` button, it will put correct spell in the box for one second and then replace with earlier text.
+
+I use local storage API to save the populated list, so when you come back again, the list will stay there.
---
+++ main.js
@@ -0,0 +1,116 @@
+var buttonToggle = document.getElementById('buttonToggle');
+var textarea = document.getElementById('list');
+var answer_box = document.getElementById('answer');
+
+function toggleVisible(e) {
+    var toHide = textarea.parentElement
+    if (toHide.classList.contains('hidden')) {
+        toHide.classList.remove('hidden');
+        buttonToggle.text = "Close List"
+    } else {
+        toHide.classList.add('hidden');
+        buttonToggle.text = "Open List"
+    }
+}
+
+buttonToggle.addEventListener('click', toggleVisible);
+
+var darkToggle = document.getElementById("darkToggle");
+darkToggle.addEventListener('click', function(e) {
+    cur = document.documentElement.dataset.theme;
+    if (cur == "dark") {
+        darkToggle.text = "Dark Theme";
+        document.documentElement.dataset.theme = "light";
+    } else {
+        darkToggle.text = "Light Theme";
+        document.documentElement.dataset.theme = "dark";
+    }
+});
+
+var spells = [];
+var current = null;
+var utterance = null;
+
+function do_play(e) {
+    if (utterance != null) {
+        speechSynthesis.speak(utterance);
+    }
+}
+
+function new_spell() {
+    if (spells.length >= 1) {
+        var idx = Math.floor(Math.random() * spells.length);
+        current = spells[idx].trim().toLowerCase();
+        utterance = new SpeechSynthesisUtterance(current);
+    }
+    if (answer_box.classList.contains('correct')) {
+        answer_box.classList.remove('correct');
+    }
+    answer_box.value = '';
+}
+
+function process_list() {
+    spells = [];
+    for (const elem of textarea.value.split('\n')) {
+        var trimmed_elem = elem.trim();
+        if (trimmed_elem != null && trimmed_elem != "") {
+            spells.push(elem.trim());    
+        }
+    }
+    new_spell();
+}
+
+function load_list_from_local_db() {
+    var list = window.localStorage.getItem("local_list");
+    if (list != null) {
+        textarea.value = list;
+        process_list();
+    }
+}
+
+function save_list_to_local_db() {
+    window.localStorage.setItem("local_list", textarea.value);
+}
+
+load_list_from_local_db();
+
+var buttonPopulate = document.getElementById("buttonPopulate");
+buttonPopulate.addEventListener('click', function(e) {
+    save_list_to_local_db();
+    process_list();
+    textarea.parentElement.classList.add('hidden');
+    buttonToggle.text = "Open List"
+});
+
+
+
+var buttonPlay = document.getElementById("buttonPlay");
+buttonPlay.addEventListener('click', do_play);
+
+function check(e) {
+    var val = answer_box.value
+    if (val.trim().toLowerCase() == current) {
+        answer_box.classList.add('correct');
+        window.setTimeout(new_spell, 1000);
+        window.setTimeout(do_play, 1500);
+    }
+}
+
+answer_box.addEventListener('keyup', check);
+
+function view_correct(e) {
+    if (current != null && current != '') {
+        var current_answer = answer_box.value;
+        answer_box.value = ''
+        answer_box.placeholder = current;
+        window.setTimeout(function() {
+            answer_box.placeholder = '';
+            answer_box.value = current_answer;
+        }, 1000);
+    }
+}
+
+document.getElementById("buttonView").addEventListener('click', view_correct);
+
+if (textarea.value != '')
+    process_list();
